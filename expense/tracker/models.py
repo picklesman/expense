9669ac0b2c918adda8b066be4a5350d5412a6d55
Elliot <elliot@ell.local>
2013-12-07 17:32:15 -0500

    add models.

--- expense/tracker/models.py
+++ expense/tracker/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return '%s' % self.name
+
+class Invoice(models.Model):
+    date = models.DateField()
+    store = models.ForeignKey(Store, null=False)
+
+    def __unicode__(self):
+        return '%s at %s on %s' % (self.total(), self.store.name, self.date)
+
+    def total(self):
+
+        all_invoices = self.item_set.all()
+
+        total = 0
+        for invoice in all_invoices:
+            total += invoice.price * invoice.quantity
+
+        return total
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    invoice = models.ForeignKey(Invoice,null=False)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    quantity = models.PositiveSmallIntegerField(default=1)
+
+    def __unicode__(self):
+        return '%s x %s' % (self.quantity, self.name)
