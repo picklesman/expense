073e07602df80ea7487b055250c34f5a7fa62105
Elliot <elliot@ell.local>
2013-12-07 17:37:30 -0500

    add new InlineModel for items in invoice.

--- expense/tracker/admin.py
+++ expense/tracker/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import Store, Invoice, Item
 # Register your models here.
 
+class ItemInline(admin.TabularInline):
+    model = Item
+    fk_name = 'invoice'
+
+class InvoiceAdmin(admin.ModelAdmin):
+   inlines = [
+           ItemInline,
+           ]
+
+
 admin.site.register(Store)
-admin.site.register(Invoice)
+admin.site.register(Invoice, InvoiceAdmin)
 admin.site.register(Item)
 
