7534a157c8fa0b5de891a18e98b5158fdd743fa6
Elliot <elliot@ell.local>
2013-12-07 17:32:36 -0500

    register models in admin.

--- expense/tracker/admin.py
+++ expense/tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Store, Invoice, Item
 # Register your models here.
+
+admin.site.register(Store)
+admin.site.register(Invoice)
+admin.site.register(Item)
+
