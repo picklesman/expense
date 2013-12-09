e08337ea3cadc7f7a0871607f9b169d6f38853c1
Elliot <elliot@ell.local>
2013-12-09 11:17:48 -0500

    add a test to make sure the total is calculated correctly and saved.

--- expense/tracker/tests.py
+++ expense/tracker/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from tracker.models import Item,Invoice,Store
 
 # Create your tests here.
+class ItemTestCase(TestCase):
+
+    def setUp(self):
+        self.store = Store.objects.create(name="Amazon")
+        self.invoice = Invoice.objects.create(store=self.store,date="2013-12-11")
+
+    def test_total_is_correct(self):
+
+        price = 20.00
+        quantity = 4
+        total = price * quantity
+
+        item = Item.objects.create(name="orange glasses", 
+                                   price=price,
+                                   quantity=quantity,
+                                   invoice=self.invoice)
+
+        self.assertEqual(item.item_total, total)
