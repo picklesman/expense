82a91faf37b5189169c8ec3f8101b1620bce269f
Elliot <elliot@ell.local>
2013-12-09 18:05:39 -0500

    heroku prep.

--- expense/expense/settings.py
+++ expense/expense/settings.py
@@ -96,3 +96,24 @@ if DEBUG:
             level = logging.INFO,
             format = '%(levelname)s %(message)s',
         )
+
+#For Heroku deployment
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
