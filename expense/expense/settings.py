d5b4687eed65b49d0b886f5c037a0b2f6a0c3f5c
Elliot <elliot@ell.local>
2013-12-09 10:25:52 -0500

    configure logging.

--- expense/expense/settings.py
+++ expense/expense/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+import logging
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -86,3 +87,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#logging
+
+if DEBUG:
+    logging.basicConfig(
+            level = logging.INFO,
+            format = '%(levelname)s %(message)s',
+        )
