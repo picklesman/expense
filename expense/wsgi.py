fbbe60f255d61198a932413157a157d3cb11b1f7
Elliot <elliot@ell.local>
2013-12-09 19:48:52 -0500

    add dj_static call.

--- expense/wsgi.py
+++ expense/wsgi.py
@@ -11,4 +11,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expense.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling #for heroku
+application = Cling(get_wsgi_application())
