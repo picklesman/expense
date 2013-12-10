6d43dc612964128d5e8810a70f8a70a89869ade0
Elliot <elliot@ell.local>
2013-12-09 19:46:33 -0500

    only do heroku config if not running on dev machine.

--- expense/settings.py
+++ expense/settings.py
@@ -102,21 +102,19 @@ if DEBUG:
 
 #For Heroku deployment
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+if not is_local:
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Static asset configuration
-#  import os
-#  BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#  STATIC_ROOT = 'staticfiles'
-#  STATIC_URL = '/static/'
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-#  STATICFILES_DIRS = (
-#      os.path.join(BASE_DIR, 'static'),
-#  )
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+
+    STATICFILES_DIRS = (
+          os.path.join(BASE_DIR, 'static'),
+    )
 
 
