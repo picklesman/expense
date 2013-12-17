a0fc8975aac885451234279d10f9ae2ab5af40f5
Elliot <elliot@ell.local>
2013-12-17 18:22:09 -0500

    add simple invoice view.

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Invoice
 
 # Create your views here.
+class InvoiceListView(ListView):
+    template_name = "invoice_list.html"
+    model = Invoice 
+
