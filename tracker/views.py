60e4f207ec24b898a291a4a25eee2f51bba8c157
Elliot <elliot@ell.local>
2013-12-17 21:27:41 -0500

    add login required decorator to dispatch method to assure login.

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from .models import Invoice
 
 # Create your views here.
@@ -7,3 +9,8 @@ class InvoiceListView(ListView):
     template_name = "invoice_list.html"
     model = Invoice 
 
+    @method_decorator(login_required(login_url='/login'))
+    def dispatch(self, *args, **kwargs):
+        return super(ListView, self).dispatch(*args, **kwargs)
+
+    
